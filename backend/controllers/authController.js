b2c06884e7af2317175717e9d1774912ebdc4e41
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-02-15 19:32:29 +0500

    Backend bcrypt to bcryptjs for deployment

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,32 +1,33 @@
-import User from '../models/User.js';
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-
+import User from "../models/User.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const registerUser = async (req, res) => {
-  try {    
-      const { username, email, password, photo } = req.body;  
-      // Check if the email is already registered
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ message: 'Email is already registered' });
-      }
-      
-      
-    // Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+  try {
+    const { username, email, password, photo } = req.body;
+    // Check if the email is already registered
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email is already registered" });
+    }
 
-      // Create a new user
-      const newUser = new User({ username, email, password: hashedPassword, photo });
-      await newUser.save();
-  
-      res.status(201).json({ message: 'User registered successfully' });
-    
-  } 
-  catch (error) {
+    // Hash the password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      photo,
+    });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
@@ -37,27 +38,35 @@ const loginUser = async (req, res) => {
     // Check if the user exists
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
-    
+
     // Compare the provided password with the stored hashed password
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid credentials' });
+      return res.status(401).json({ message: "Invalid credentials" });
     }
     const { password: _, role, ...rest } = user._doc;
     // Generate a JWT token
     // const token = jwt.sign({ id: user._id, role: user.role }, process.env.SECRET_KEY, { expiresIn: '5d' });
-    const token = jwt.sign({ id: user._id, role: user.role }, process.env.SECRET_KEY, { expiresIn: '5d' });;
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
 
     // Set the token as a cookie in the response
-    res.cookie('accessToken', token, { httpOnly: true, expires:token.expiresIn })
+    res.cookie("accessToken", token, {
+      httpOnly: true,
+      expires: token.expiresIn,
+    });
 
-
-    res.status(200).json({ message: 'Login successful', data:{...rest}, token, role });
+    res
+      .status(200)
+      .json({ message: "Login successful", data: { ...rest }, token, role });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
