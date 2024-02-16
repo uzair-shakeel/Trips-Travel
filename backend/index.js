e2c1f11b03797989e421b322e0a3235957137987
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-02-17 00:34:11 +0500

    .

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,50 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import tourRoutes from './routes/tourRoutes.js';
-import reviewRoutes from './routes/reviewRoutes.js';
-import bookingRoutes from './routes/bookingRoutes.js';
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import tourRoutes from "./routes/tourRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3050;
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("DB connected"))
-.catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
-app.use(cors());
-app.use(express.json())
-app.use(cookieParser())
+// Middleware for CORS and JSON parsing
+const allowedOrigins = ["https://trips-travel.vercel.app"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 app.use(bodyParser.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/tour', tourRoutes);
-app.use('/api/review', reviewRoutes);
-app.use('/api/booking', bookingRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/tour", tourRoutes);
+app.use("/api/review", reviewRoutes);
+app.use("/api/booking", bookingRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Trips & Travels API!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Trips & Travels API!");
 });
 
 app.listen(PORT, () => {
--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://localhost:3050/api';
-// 
+const BASE_URL = "https://trips-travel-server.vercel.app";
+//
 export default BASE_URL;
